f521046492ee6a91b03e579e5675fcee22c3eb54
Francisco, Johannes D. <johannesfrancisco50@gmail.com>
2024-11-06 23:51:34 +0800

    chore: update "python dictionaries data structure"

--- app.py
+++ app.py
@@ -6,10 +6,19 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html', message="Hello, World!")
 
+# class for product dictionary
 class Product:
     def __init__(self, name, price):
         self.name = name
         self.price = price
+
+# class for employee dictionary
+class Employee:
+    def __init__(self, name, department):
+        self.name = name
+        self.department = department
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
