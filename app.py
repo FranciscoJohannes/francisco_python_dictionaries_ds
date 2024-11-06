f7ad9d74e3d6ca497ca9b092f05b61cd05a795b2
Francisco, Johannes D. <johannesfrancisco50@gmail.com>
2024-11-06 23:47:02 +0800

    chore: python dictionaries data structure

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html', message="Hello, World!")
+
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+if __name__ == '__main__':
+    app.run(debug=True)
